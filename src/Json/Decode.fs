23f0513e58972277e29dfd5dda1823fee1c134cc
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-11-13 20:55:30 +0100

    wip

--- src/Json/Decode.fs
+++ src/Json/Decode.fs
@@ -3,65 +3,72 @@ module Thot.Json.Decode
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
-
-[<Pojo>]
-type DecoderError =
-    { Message : string
-      Parents : DecoderError list
-      Value : obj }
-
-let makeErr message value parents =
-    Error({ Message = message
-            Parents = parents
-            Value = value })
-
-type Decoder<'T> = obj -> Result<'T, DecoderError>
+open System.Net.Http
 
 module Helpers =
 
     [<Emit("typeof $0 === 'string'")>]
-    let isString (input : obj) : bool = jsNative
+    let isString (_ : obj) : bool = jsNative
+
+    [<Emit("typeof $0 === 'boolean'")>]
+    let isBoolean (_ : obj) : bool = jsNative
 
     [<Emit("typeof $0 === 'number'")>]
-    let isNumber (input : obj) : bool = jsNative
+    let isNumber (_ : obj) : bool = jsNative
 
     [<Emit("Number.isNaN($0)")>]
-    let isNaN (x: obj) : bool = jsNative
+    let isNaN (_: obj) : bool = jsNative
+
+    [<Emit("-2147483647 < $0 && $0 < 2147483647 && ($0 | 0) === $0")>]
+    let isValidIntRange (_: obj) : bool = jsNative
+
+    [<Emit("isFinite($0) && !($0 % 1)")>]
+    let isIntFinite (_: obj) : bool = jsNative
 
     [<Emit("($0 !== undefined)")>]
-    let isDefined (o: obj) : bool = jsNative
+    let isDefined (_: obj) : bool = jsNative
 
-let string (value: obj) : Result<string, DecoderError> =
-    if Helpers.isString value then
-        Ok(unbox<string> value)
-    else
-        makeErr "a string" value []
+    [<Emit("JSON.stringify($0) + ''")>]
+    let anyToString (_: obj) : string= jsNative
 
+type PrimitiveError =
+    { Msg : string
+      Value : obj }
 
-let float (value: obj) : Result<float, DecoderError> =
-    if Helpers.isNumber value then
-        Ok(unbox<float> value)
-    else
-        printfn "error: %A" value
-        makeErr "a float" value []
+type DecoderError =
+    | BadPrimitive of string * obj
+    | BadPrimitiveExtra of string * obj * string
+    | BadField of string * obj
 
-let field (fieldName: string) (decoder : Decoder<'value>) (value: obj) : Result<'value, DecoderError> =
-    try
-        let fieldValue = value?(fieldName)
-        if Helpers.isDefined fieldValue then
-            decoder fieldValue
-        else
-            makeErr ("an object with a field named `" + fieldName + "`") value []
-    with
-        | _ ->
-            makeErr ("an object with a field named `" + fieldName + "`") value []
+type Decoder<'T> = obj -> Result<'T, DecoderError>
 
-let decode (decoder : Decoder<'T>) (value : obj) : Result<'T, string> =
+let inline genericMsg msg value = "Expecting " + msg + " but instead got: " + (Helpers.anyToString value)
+
+let errorToString =
+    function
+    | BadPrimitive (msg, value) ->
+        genericMsg msg value
+    | BadPrimitiveExtra (msg, value, reason) ->
+        genericMsg msg value + ". Reason: " + reason
+    | BadField (msg, value) ->
+        genericMsg msg value
+
+let unwrap (decoder : Decoder<'T>) (value : obj) : 'T =
     match decoder value with
     | Ok success ->
-        Ok success
+        success
     | Error error ->
-        Error error.Message
+        failwith (errorToString error)
+
+let decode (decoder : Decoder<'T>) (value : obj) : Result<'T, string> =
+    try
+        match decoder value with
+        | Ok success ->
+            Ok success
+        | Error error ->
+            Error (errorToString error)
+    with
+        | ex -> Error ex.Message
 
 let decodeString (decoder : Decoder<'T>) (value : string) : Result<'T, string> =
     try
@@ -71,17 +78,136 @@ let decodeString (decoder : Decoder<'T>) (value : string) : Result<'T, string> =
         | ex ->
             Error("Given an invalid JSON: " + ex.Message)
 
-let unwrap (decoder : Decoder<'T>) (value : obj) : 'T =
-    match decoder value with
-    | Ok success ->
-        success
-    | Error error ->
-        failwith error.Message
+let string (value: obj) : Result<string, DecoderError> =
+    if Helpers.isString value then
+        Ok(unbox<string> value)
+    else
+        BadPrimitive("a string", value) |> Error
 
-let map2 (ctor : 'a -> 'b -> 'value) (d1 : Decoder<'a>) (d2 : Decoder<'b>) : Decoder<'value> =
+let int (value: obj) : Result<int, DecoderError> =
+    if not (Helpers.isNumber value)  then
+        BadPrimitive("an int", value) |> Error
+    else
+        if not (Helpers.isValidIntRange value) then
+            BadPrimitiveExtra("an int", value, "Invalid range") |> Error
+        else
+            Ok(unbox<int> value)
+
+let nil (output : obj) (value: obj) : Result<obj, DecoderError> =
+    if isNull value then
+        Ok output
+    else
+        BadPrimitive("null", value) |> Error
+
+let bool (value: obj) : Result<bool, DecoderError> =
+    if Helpers.isBoolean value then
+        Ok(unbox<bool> value)
+    else
+        BadPrimitive("a boolean", value) |> Error
+
+let float (value: obj) : Result<float, DecoderError> =
+    if Helpers.isNumber value then
+        Ok(unbox<float> value)
+    else
+        BadPrimitive("a float", value) |> Error
+
+let field (fieldName: string) (decoder : Decoder<'value>) (value: obj) : Result<'value, DecoderError> =
+    let fieldValue = value?(fieldName)
+    if Helpers.isDefined fieldValue then
+        decoder fieldValue
+    else
+        BadField ("an object with a field named `" + fieldName + "`", value)
+        |> Error
+
+// let nullable (d1: Decoder<'value>) : Resul<'value option, DecoderError> =
+
+let optional (d1 : Decoder<'value>) (value: obj) :Result<'value option, DecoderError> =
+    match decode d1 value with
+    | Ok v -> Ok (Some v)
+    | Error _ -> Ok None
+
+// Map functions
+
+let map ctor d1 =
+    (fun value ->
+        let t = unwrap d1 value
+        Ok (ctor t)
+    )
+
+let map2 ctor d1 d2 =
     (fun value ->
         let t = unwrap d1 value
         let t2 = unwrap d2 value
 
         Ok (ctor t t2)
+    )
+
+let map3 ctor d1 d2 d3 =
+    (fun value ->
+        let v1 = unwrap d1 value
+        let v2 = unwrap d2 value
+        let v3 = unwrap d3 value
+
+        Ok (ctor v1 v2 v3)
+    )
+
+let map4 ctor d1 d2 d3 d4 =
+    (fun value ->
+        let v1 = unwrap d1 value
+        let v2 = unwrap d2 value
+        let v3 = unwrap d3 value
+        let v4 = unwrap d4 value
+
+        Ok (ctor v1 v2 v3 v4)
+    )
+
+let map5 ctor d1 d2 d3 d4 d5=
+    (fun value ->
+        let v1 = unwrap d1 value
+        let v2 = unwrap d2 value
+        let v3 = unwrap d3 value
+        let v4 = unwrap d4 value
+        let v5 = unwrap d5 value
+
+        Ok (ctor v1 v2 v3 v4 v5)
+    )
+
+let map6 ctor d1 d2 d3 d4 d5 d6 =
+    (fun value ->
+        let v1 = unwrap d1 value
+        let v2 = unwrap d2 value
+        let v3 = unwrap d3 value
+        let v4 = unwrap d4 value
+        let v5 = unwrap d5 value
+        let v6 = unwrap d6 value
+
+        Ok (ctor v1 v2 v3 v4 v5 v6)
+    )
+
+let map7 ctor d1 d2 d3 d4 d5 d6 d7 =
+    (fun value ->
+        let v1 = unwrap d1 value
+        let v2 = unwrap d2 value
+        let v3 = unwrap d3 value
+        let v4 = unwrap d4 value
+        let v5 = unwrap d5 value
+        let v6 = unwrap d6 value
+        let v7 = unwrap d7 value
+
+        Ok (ctor v1 v2 v3 v4 v5 v6 v7)
+    )
+
+
+let map8 ctor d1 d2 d3 d4 d5 d6 d7 d8 =
+    (fun value ->
+        let v1 = unwrap d1 value
+        let v2 = unwrap d2 value
+        let v3 = unwrap d3 value
+        let v4 = unwrap d4 value
+        let v5 = unwrap d5 value
+        let v6 = unwrap d6 value
+        let v7 = unwrap d7 value
+        let v8 = unwrap d8 value
+
+        Ok (ctor v1 v2 v3 v4 v5 v6 v7 v8)
     )
--- tests/Tests.Json.Decode.fs
+++ tests/Tests.Json.Decode.fs
@@ -12,45 +12,373 @@ let it (msg: string) (f: unit->unit): unit = jsNative
 [<Global>]
 let describe (msg: string) (f: unit->unit): unit = jsNative
 
-type Point =
-    { x : float
-      y : float }
+type Record2 =
+    { a : float
+      b : float }
 
-    static member Create x y =
-        { x = x
-          y = y }
+    static member Create a b =
+        { a = a
+          b = b }
+
+type Record3 =
+    { a : float
+      b : float
+      c : float }
+
+    static member Create a b c =
+        { a = a
+          b = b
+          c = c }
+
+type Record4 =
+    { a : float
+      b : float
+      c : float
+      d : float }
+
+    static member Create a b c d =
+        { a = a
+          b = b
+          c = c
+          d = d }
+
+type Record5 =
+    { a : float
+      b : float
+      c : float
+      d : float
+      e : float }
+
+    static member Create a b c d e =
+        { a = a
+          b = b
+          c = c
+          d = d
+          e = e }
+
+type Record6 =
+    { a : float
+      b : float
+      c : float
+      d : float
+      e : float
+      f : float }
+
+    static member Create a b c d e f =
+        { a = a
+          b = b
+          c = c
+          d = d
+          e = e
+          f = f }
+
+type Record7 =
+    { a : float
+      b : float
+      c : float
+      d : float
+      e : float
+      f : float
+      g : float }
+
+    static member Create a b c d e f g =
+        { a = a
+          b = b
+          c = c
+          d = d
+          e = e
+          f = f
+          g = g }
+
+type Record8 =
+    { a : float
+      b : float
+      c : float
+      d : float
+      e : float
+      f : float
+      g : float
+      h : float }
+
+    static member Create a b c d e f g h =
+        { a = a
+          b = b
+          c = c
+          d = d
+          e = e
+          f = f
+          g = g
+          h = h }
+
+let jsonRecord =
+    """{ "a": 1,
+         "b": 2,
+         "c": 3,
+         "d": 4,
+         "e": 5,
+         "f": 6,
+         "g": 7,
+         "h": 8 }"""
 
 describe "Decode" <| fun _ ->
 
-    it "a string works" <| fun _ ->
-        let expected = Ok("maxime")
-        let actual =
-            decodeString string "\"maxime\""
+    describe "Primitives: " <| fun _ ->
 
-        // Fable.Import.JS.console.log (Ok("maxime"))
-        // Fable.Import.JS.console.log (Error("maxime"))
+        it "a string works" <| fun _ ->
+            let expected = Ok("maxime")
+            let actual =
+                decodeString string "\"maxime\""
 
-        Assert.AreEqual(expected, actual)
+            Assert.AreEqual(expected, actual)
 
-    it "a float works" <| fun _ ->
-        let expected = Ok(1.2)
-        let actual =
-            decodeString float "1.2"
+        it "a float works" <| fun _ ->
+            let expected = Ok(1.2)
+            let actual =
+                decodeString float "1.2"
 
-        // Fable.Import.JS.console.log (Ok("maxime"))
-        // Fable.Import.JS.console.log (Error("maxime"))
+            Assert.AreEqual(expected, actual)
 
-        Assert.AreEqual(expected, actual)
+        it "null works" <| fun _ ->
+            let expected = Ok(20 :> obj)
+            let actual =
+                decodeString (nil 20) "null"
 
-    it "an object works" <| fun _ ->
-        let expected = Ok({x = 3.; y = 4.} : Point)
+            Assert.AreEqual(expected, actual)
 
-        let decodePoint =
-            map2 Point.Create
-                (field "x" float)
-                (field "y" float)
+        it "null works" <| fun _ ->
+            let expected = Ok(false :> obj)
+            let actual =
+                decodeString (nil false) "null"
 
-        let actual =
-            decodeString decodePoint """{ "x": 3, "y": 4 }"""
+            Assert.AreEqual(expected, actual)
 
-        Assert.AreEqual(expected, actual)
+        it "a bool works" <| fun _ ->
+            let expected = Ok(true)
+            let actual =
+                decodeString bool "true"
+
+            Assert.AreEqual(expected, actual)
+
+        it "an int works" <| fun _ ->
+            let expected = Ok(25)
+            let actual =
+                decodeString int "25"
+
+            Assert.AreEqual(expected, actual)
+
+        it "an invalid int [invalid range] output an error" <| fun _ ->
+            let expected = Error("Expecting an int but instead got: 2147483648. Reason: Invalid range")
+            let actual =
+                decodeString int "2147483648"
+
+            Assert.AreEqual(expected, actual)
+
+        it "an invalid int [invalid range] output an error" <| fun _ ->
+            let expected = Error("Expecting an int but instead got: -2147483648. Reason: Invalid range")
+            let actual =
+                decodeString int "-2147483648"
+
+            Assert.AreEqual(expected, actual)
+
+    describe "Data structure" <| fun _ ->
+
+        // it "nullable works" <| fun _ ->
+        //     let expected = Ok({a = 1.; b = 2.} : Record2)
+
+        //     let decodePoint =
+        //         map2 Record2.Create
+        //             (field "a" float)
+        //             (field "b" float)
+
+        //     let actual =
+        //         decodeString decodePoint jsonRecord
+
+        //     Assert.AreEqual(expected, actual)
+
+    describe "Inconsistent structure" <| fun _ ->
+
+        it "optional works" <| fun _ ->
+            let json = """{ "name": "maxime", "age": 25 }"""
+
+            let expectedValid = Ok(Some "maxime")
+            let actualValid =
+                decodeString (optional (field "name" string) ) json
+
+            Assert.AreEqual(expectedValid, actualValid)
+
+            let expectedInvalidType = Ok(None)
+            let actualInvalidType =
+                decodeString (optional (field "name" int) ) json
+
+            Assert.AreEqual(expectedInvalidType, actualInvalidType)
+
+            let expectedMissingField = Ok(None)
+            let actualMissingField =
+                decodeString (optional (field "height" int) ) json
+
+            Assert.AreEqual(expectedMissingField, actualMissingField)
+
+            let expectedFieldAndThenOptional = Error("Expecting an object with a field named `height` but instead got: {\"name\":\"maxime\",\"age\":25}")
+            let actualFieldAndThenOptional =
+                decodeString (field "height" (optional float)) json
+
+            Assert.AreEqual(expectedFieldAndThenOptional, actualFieldAndThenOptional)
+
+        it "optional works" <| fun _ ->
+            let expected = Ok(Some "maxime")
+            let json = """{ "name": "maxime", "age": 25 }"""
+
+            let actual =
+                decodeString (optional (field "name" string) ) json
+
+            Assert.AreEqual(expected, actual)
+
+    describe "Mapping" <| fun _ ->
+
+        it "map works" <| fun _ ->
+            let expected = Ok(6)
+            let stringLength =
+                map String.length string
+
+            let actual =
+                decodeString stringLength "\"maxime\""
+            Assert.AreEqual(expected, actual)
+
+        it "map2 works" <| fun _ ->
+            let expected = Ok({a = 1.; b = 2.} : Record2)
+
+            let decodePoint =
+                map2 Record2.Create
+                    (field "a" float)
+                    (field "b" float)
+
+            let actual =
+                decodeString decodePoint jsonRecord
+
+            Assert.AreEqual(expected, actual)
+
+        it "map3 works" <| fun _ ->
+            let expected = Ok({ a = 1.
+                                b = 2.
+                                c = 3. } : Record3)
+
+            let decodePoint =
+                map3 Record3.Create
+                    (field "a" float)
+                    (field "b" float)
+                    (field "c" float)
+
+            let actual =
+                decodeString decodePoint jsonRecord
+
+            Assert.AreEqual(expected, actual)
+
+        it "map4 works" <| fun _ ->
+            let expected = Ok({ a = 1.
+                                b = 2.
+                                c = 3.
+                                d = 4. } : Record4)
+
+            let decodePoint =
+                map4 Record4.Create
+                    (field "a" float)
+                    (field "b" float)
+                    (field "c" float)
+                    (field "d" float)
+
+            let actual =
+                decodeString decodePoint jsonRecord
+
+            Assert.AreEqual(expected, actual)
+
+        it "map5 works" <| fun _ ->
+            let expected = Ok({ a = 1.
+                                b = 2.
+                                c = 3.
+                                d = 4.
+                                e = 5. } : Record5)
+
+            let decodePoint =
+                map5 Record5.Create
+                    (field "a" float)
+                    (field "b" float)
+                    (field "c" float)
+                    (field "d" float)
+                    (field "e" float)
+
+            let actual =
+                decodeString decodePoint jsonRecord
+
+            Assert.AreEqual(expected, actual)
+
+        it "map6 works" <| fun _ ->
+            let expected = Ok({ a = 1.
+                                b = 2.
+                                c = 3.
+                                d = 4.
+                                e = 5.
+                                f = 6. } : Record6)
+
+            let decodePoint =
+                map6 Record6.Create
+                    (field "a" float)
+                    (field "b" float)
+                    (field "c" float)
+                    (field "d" float)
+                    (field "e" float)
+                    (field "f" float)
+
+            let actual =
+                decodeString decodePoint jsonRecord
+
+            Assert.AreEqual(expected, actual)
+
+        it "map7 works" <| fun _ ->
+            let expected = Ok({ a = 1.
+                                b = 2.
+                                c = 3.
+                                d = 4.
+                                e = 5.
+                                f = 6.
+                                g = 7. } : Record7)
+
+            let decodePoint =
+                map7 Record7.Create
+                    (field "a" float)
+                    (field "b" float)
+                    (field "c" float)
+                    (field "d" float)
+                    (field "e" float)
+                    (field "f" float)
+                    (field "g" float)
+
+            let actual =
+                decodeString decodePoint jsonRecord
+
+            Assert.AreEqual(expected, actual)
+
+        it "map8 works" <| fun _ ->
+            let expected = Ok({ a = 1.
+                                b = 2.
+                                c = 3.
+                                d = 4.
+                                e = 5.
+                                f = 6.
+                                g = 7.
+                                h = 8. } : Record8)
+
+            let decodePoint =
+                map8 Record8.Create
+                    (field "a" float)
+                    (field "b" float)
+                    (field "c" float)
+                    (field "d" float)
+                    (field "e" float)
+                    (field "f" float)
+                    (field "g" float)
+                    (field "h" float)
+
+            let actual =
+                decodeString decodePoint jsonRecord
+
+            Assert.AreEqual(expected, actual)
