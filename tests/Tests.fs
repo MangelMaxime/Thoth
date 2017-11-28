42d807a21950c0b430223d12c5133989e3fcd014
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-11-28 15:53:16 +0100

    Re-activate the encode tests

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -4,4 +4,4 @@ open Fable.Core.JsInterop
 
 // This is necessary to make webpack collect all test files
 importSideEffects "./Tests.Json.Decode.fs"
-// importSideEffects "./Tests.Json.Encode.fs"
+importSideEffects "./Tests.Json.Encode.fs"
