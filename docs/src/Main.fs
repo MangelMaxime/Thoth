9e30d62ecb47041d5ca52aa94064e363a7a1e9e3
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-02-27 10:24:59 +0100

    Comment Http docs

--- docs/src/Main.fs
+++ docs/src/Main.fs
@@ -5,5 +5,5 @@ open Renderer
 FromMarkdown.render (Route.Index) "Index.md"
 FromMarkdown.render (Route.Json.Encode) "Json/Encode.md"
 FromMarkdown.render (Route.Json.Decode) "Json/Decode.md"
-FromMarkdown.render (Route.Http.Basic) "Http/Basic.md"
-FromMarkdown.render (Route.Http.Elmish) "Http/Elmish.md"
+// FromMarkdown.render (Route.Http.Basic) "Http/Basic.md"
+// FromMarkdown.render (Route.Http.Elmish) "Http/Elmish.md"
