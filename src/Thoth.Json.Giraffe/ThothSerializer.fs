fbef68a58ed231654557300d5eaae59f8f711650
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-10-02 11:18:02 +0200

    Release Thoth.Json.Giraffe 1.1.0

--- src/Thoth.Json.Giraffe/ThothSerializer.fs
+++ src/Thoth.Json.Giraffe/ThothSerializer.fs
@@ -1,26 +1,37 @@
 namespace Thoth.Json.Giraffe
 
 open System.IO
-open Giraffe
 open Giraffe.Serialization.Json
 open Thoth.Json.Net
+open System.Text
+open System.Threading.Tasks
 
 type ThothSerializer () =
+    let Utf8EncodingWithoutBom = new UTF8Encoding(false)
+    let DefaultBufferSize = 1024
+
     interface IJsonSerializer with
-        member __.Serialize (o : obj) =
+        member __.SerializeToString (o : 'T) =
             Encode.Auto.toString(0, o)
 
         member __.Deserialize<'T> (json : string) =
             Decode.Auto.unsafeFromString<'T>(json)
 
-        member __.Deserialize<'T> (stream : Stream) =
-            use sr = new StreamReader(stream, true)
-            let str = sr.ReadToEnd()
-            Decode.Auto.unsafeFromString<'T>(str)
+        member __.Deserialize<'T> (bytes : byte[]) =
+            let json =  Encoding.UTF8.GetString bytes
+            Decode.Auto.unsafeFromString<'T>(json)
 
         member __.DeserializeAsync<'T> (stream : Stream) =
-            task {
-                use sr = new StreamReader(stream, true)
-                let str = sr.ReadToEnd()
-                return Decode.Auto.unsafeFromString<'T>(str)
-            }
+            use sr = new StreamReader(stream, true)
+            let str = sr.ReadToEnd()
+            Task.FromResult(Decode.Auto.unsafeFromString<'T>(str))
+
+        member __.SerializeToBytes<'T>(o : 'T) : byte array =
+            Encode.Auto.toString(0, o)
+            |> Encoding.UTF8.GetBytes
+
+        member __.SerializeToStreamAsync (o : 'T) (stream : Stream) =
+            use sw = new StreamWriter(stream, Utf8EncodingWithoutBom, DefaultBufferSize, true)
+            let json = Encode.Auto.toString(0, o)
+            sw.Write(json)
+            Task.CompletedTask
